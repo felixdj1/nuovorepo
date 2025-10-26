93e9e8982ac660c08e52f3258512b3e8e3561832
felixdj1 <15077256+felixdj1@users.noreply.github.com>
2025-10-26 17:09:49 +0100

    Aggiornato README e aggiunto script run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Vai nella cartella della web app
+cd ~/nuovorepo/deepsite
+
+# Installa le dipendenze Node.js (solo se non già installate)
+npm install
+
+# Avvia il progetto in modalità sviluppo
+npm run dev
